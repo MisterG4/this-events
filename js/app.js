86ad2da3e715ea3d6490e27f1bebd3b250353c57
Anthony Garingan <anthonygaringan@Anthony-Garigan.local>
2017-02-23 21:49:33 -1000

    working on starbucks now

--- js/app.js
+++ js/app.js
@@ -2,3 +2,17 @@
 /*Add event listeners to each restaurant name that will show and hide (toggle) the secret menu item for that particular restaurant.*/
 
 
+var secretMenu = document.getElementsByClassName("name");
+for(var i = 0;i<secretMenu.length;i++){
+secretMenu[i].addEventListener("click", showSecret);
+
+}
+
+function showSecret(){
+	var theGoods = this.querySelectorAll(".menu")[0];
+	if(theGoods.style.display === "none"){
+		theGoods.style.display = "block";
+	}else{
+		theGoods.style.display = "none";
+	}
+}
